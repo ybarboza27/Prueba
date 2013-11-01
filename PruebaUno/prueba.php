26d1d702aba30f46db429e5aaf06d47d68132436
mau <mau@mau-PC>
2013-11-01 09:15:43 -0600

    introduje la clase para la calculadora

---
+++ PruebaUno/prueba.php
@@ -0,0 +1,34 @@
+<?php
+
+class prueba{
+    
+    public function sumar($numeroUno,$numeroDos) {
+        
+        return $numeroUno  + $numeroDos;
+        
+    }
+    
+    public function restar($numeroUno,$numeroDos) {
+        
+        return $numeroUno  - $numeroDos;
+        
+    }
+    
+    public function multipliccar($numeroUno,$numeroDos) {
+        
+        return $numeroUno  * $numeroDos;
+        
+    }
+    
+    public function dividir($numeroUno,$numeroDos) {
+        
+        return $numeroUno  / $numeroDos;
+        
+    }
+
+    
+}
+
+
+
+?>
